a7f2b1e1c8ff58cc717c0a890a817529c0af871a
frozenfung <frozenfung@gmail.com>
2024-01-04 13:40:41 +0800

    Initial Commit

---
+++ HotDamnClockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotDamnClockApp
+//
+//  Created by frozenfung on 2024/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
